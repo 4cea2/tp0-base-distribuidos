68ecbbf9dbf3aafcd7deeaca6d199e9fbc863fd8
4cea2 <kcarbajal@fi.uba.ar>
2026-03-16 23:25:45 -0300

    feat: implement mi-generador.py

--- mi-generador.py
+++ mi-generador.py
@@ -1,1 +1,45 @@
-print("Hola mundo")
+import sys
+
+output_file = sys.argv[1]
+num_clients = int(sys.argv[2])
+
+with open(output_file, "w") as f:
+    # Server
+    f.write("""name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+""")
+
+    # Clients
+    for i in range(1, num_clients + 1):
+        f.write(f"""
+  client{i}:
+    container_name: client{i}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID={i}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+""")
+
+    # Networks
+    f.write("""
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+""")
