bb7a4aa803650380595f2ea8bce3482cbe20e4a3
4cea2 <kcarbajal@fi.uba.ar>
2026-03-18 23:25:25 -0300

    feat: add graceful shutdown handling with SIGTERM in server

--- server/common/server.py
+++ server/common/server.py
@@ -1,5 +1,6 @@
 import socket
 import logging
+import signal
 
 
 class Server:
@@ -8,6 +9,14 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._running = True
+        signal.signal(signal.SIGTERM, self._handle_sigterm)
+
+    def _handle_sigterm(self, signum, frame):
+        self._running = False
+        self._server_socket.close()
+        logging.info('action: shutdown | result: success | signal: SIGTERM')
+
 
     def run(self):
         """
@@ -20,7 +29,7 @@ class Server:
 
         # TODO: Modify this program to handle signal to graceful shutdown
         # the server
-        while True:
+        while self._running:
             client_sock = self.__accept_new_connection()
             self.__handle_client_connection(client_sock)
 
