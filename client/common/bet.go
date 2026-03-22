c5947c9cc78e030e6a59467743964381d9fb4939
4cea2 <kcarbajal@fi.uba.ar>
2026-03-21 21:09:38 -0300

    feat(client): add Bet struct and load from env variables

---
+++ client/common/bet.go
@@ -0,0 +1,58 @@
+package common
+
+import (
+	"bytes"
+	"encoding/binary"
+	"os"
+)
+
+// Bet struct that represents a bet to be sent to the server
+type Bet struct {
+	Agency    int
+	FirstName string
+	LastName  string
+	Document  int
+	BirthDate string
+	Number    int
+}
+
+// loadBetFromEnv reads, validates and parses env variables
+func loadBetFromEnv() (string, string, int, string, int, error) {
+	firstName := os.Getenv("NOMBRE")
+	lastName := os.Getenv("APELLIDO")
+	documentStr := os.Getenv("DOCUMENTO")
+	birthDate := os.Getenv("NACIMIENTO")
+	numberStr := os.Getenv("NUMERO")
+
+	if firstName == "" || lastName == "" || documentStr == "" || birthDate == "" || numberStr == "" {
+		return "", "", 0, "", 0, fmt.Errorf("missing required environment variables")
+	}
+
+	document, err := strconv.Atoi(documentStr)
+	if err != nil {
+		return "", "", 0, "", 0, fmt.Errorf("invalid DOCUMENTO: %w", err)
+	}
+
+	number, err := strconv.Atoi(numberStr)
+	if err != nil {
+		return "", "", 0, "", 0, fmt.Errorf("invalid NUMERO: %w", err)
+	}
+
+	return firstName, lastName, document, birthDate, number, nil
+}
+
+func NewBet(agency int) (*Bet, error) {
+	firstName, lastName, document, birthDate, number, err := loadBetFromEnv()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Bet{
+		Agency:    agency,
+		FirstName: firstName,
+		LastName:  lastName,
+		Document:  document,
+		BirthDate: birthDate,
+		Number:    number,
+	}, nil
+}
