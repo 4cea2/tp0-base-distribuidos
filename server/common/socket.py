c60d788e92b5fc7787d4c47b5574a6bfc914dfe1
4cea2 <kcarbajal@fi.uba.ar>
2026-03-22 21:30:09 -0300

    feat(server): wrap accepted connections into Socket

--- server/common/socket.py
+++ server/common/socket.py
@@ -2,10 +2,15 @@ import socket
 
 
 class Socket:
-    def __init__(self, port: str, listen_backlog: int):
-        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._sock.bind(('', port))
-        self._sock.listen(listen_backlog)
+    def __init__(self, port: int = None, listen_backlog: int = None, sock=None):
+        if sock is not None:
+            # Socket is already created, use it
+            self._sock = sock
+        else:
+            # Socket is not created, create it
+            self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._sock.bind(('', port))
+            self._sock.listen(listen_backlog)
 
     def send(self, data: bytes) -> None:
         self._sock.sendall(data)
@@ -20,6 +25,10 @@ class Socket:
             data += n
             total += len(n)
         return data
+    
+    def accept(self):
+        c, addr = self._sock.accept()
+        return Socket(sock=c), addr
 
     def close(self) -> None:
         self._sock.close()
