3467a667d125c64957ec2dda98e6de9737bfead1
4cea2 <kcarbajal@fi.uba.ar>
2026-03-23 17:56:58 -0300

    refactor: add envs vars to generator

--- mi-generador.py
+++ mi-generador.py
@@ -28,8 +28,11 @@ services:
     entrypoint: /client
     environment:
       - CLI_ID={i}
-    env_file:
-      - ./client/.env
+      - NOMBRE=Santiago Lionel
+      - APELLIDO=Lorca
+      - DOCUMENTO=30904465
+      - NACIMIENTO=1999-03-17
+      - NUMERO=7574
     networks:
       - testing_net
     depends_on:
