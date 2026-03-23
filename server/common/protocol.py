3cff88452bfa9e05d3f51d177b3ba3cbc8093318
4cea2 <kcarbajal@fi.uba.ar>
2026-03-23 18:23:43 -0300

    refactor(server): remove magic numbers

--- server/common/protocol.py
+++ server/common/protocol.py
@@ -2,7 +2,12 @@ from common.utils import Bet
 
 TYPE_INT = 1
 TYPE_STRING = 2
+
 CONFIRMATION_BET = 0
+
+TYPE_SIZE = 1
+INT_SIZE = 4
+STRING_LENGTH_SIZE = 2
 
 class Protocol:
     def __init__(self, sock):
@@ -12,7 +17,7 @@ class Protocol:
         """
         Read a byte and check if it matches the expected type
         """
-        t = self._sock.receive(1)[0]
+        t = self._sock.receive(TYPE_SIZE)[0]
         if t != expected:
             raise RuntimeError(f"Invalid type: expected {expected}, got {t}")
 
@@ -20,14 +25,14 @@ class Protocol:
         """
         Read 4 bytes and convert to int
         """
-        data = self._sock.receive(4)
+        data = self._sock.receive(INT_SIZE)
         return int.from_bytes(data, byteorder="big")
 
     def _read_string(self) -> str:
         """
         Read 2 bytes to get the length, then read the string of that length
         """
-        length_bytes = self._sock.receive(2)
+        length_bytes = self._sock.receive(STRING_LENGTH_SIZE)
         length = int.from_bytes(length_bytes, byteorder="big")
         data = self._sock.receive(length)
         return data.decode()
