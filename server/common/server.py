09ad749c85a113d68b25153f506586d49263beec
4cea2 <kcarbajal@fi.uba.ar>
2026-03-23 16:54:15 -0300

    feat(server): receive Bet from client and store

--- server/common/server.py
+++ server/common/server.py
@@ -1,14 +1,13 @@
 import socket
 import logging
 import signal
-
+from common.socket import Socket
+from common.protocol import Protocol
+import common.utils as utils
 
 class Server:
     def __init__(self, port, listen_backlog):
-        # Initialize server socket
-        self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._server_socket.bind(('', port))
-        self._server_socket.listen(listen_backlog)
+        self._server_socket = Socket(port, listen_backlog)
         self._running = True
         self._client_socket = None
         signal.signal(signal.SIGTERM, self._handle_sigterm)
@@ -21,14 +20,6 @@ class Server:
 
 
     def run(self):
-        """
-        Dummy Server loop
-
-        Server that accept a new connections and establishes a
-        communication with a client. After client with communucation
-        finishes, servers starts to accept new connections again
-        """
-
         # TODO: Modify this program to handle signal to graceful shutdown
         # the server
         while self._running:
@@ -36,35 +27,22 @@ class Server:
             if self._client_socket is None:
                 break
             self.__handle_client_connection()
+        self._server_socket.close()
 
     def __handle_client_connection(self):
-        """
-        Read message from a specific client socket and closes the socket
-
-        If a problem arises in the communication with the client, the
-        client socket will also be closed
-        """
+        protocol = Protocol(self._client_socket)
         try:
-            # TODO: Modify the receive to avoid short-reads
-            msg = self._client_socket.recv(1024).rstrip().decode('utf-8')
-            addr = self._client_socket.getpeername()
-            logging.info(f'action: receive_message | result: success | ip: {addr[0]} | msg: {msg}')
-            # TODO: Modify the send to avoid short-writes
-            self._client_socket.send("{}\n".format(msg).encode('utf-8'))
+            bet = protocol.receive_bet()
+            logging.info(f'action: receive_bet | result: success | bet: {bet.__dict__}')
+            utils.store_bets([bet])
+            logging.info(f'action: store_bet | result: success | bet: {bet.__dict__}')
         except OSError as e:
-            logging.error("action: receive_message | result: fail | error: {e}")
+            logging.error("action: receive_bet | result: fail | error: {e}")
         finally:
             if self._running:
                 self._client_socket.close()
 
     def __accept_new_connection(self):
-        """
-        Accept new connections
-
-        Function blocks until a connection to a client is made.
-        Then connection created is printed and returned
-        """
-
         # Connection arrived
         logging.info('action: accept_connections | result: in_progress')
         try:
