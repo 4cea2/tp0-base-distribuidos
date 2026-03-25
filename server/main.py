59dcab7e4c553b6e080ef431701dccbcee7bf5af
4cea2 <kcarbajal@fi.uba.ar>
2026-03-25 01:32:04 -0300

    feat(server): initialize with the expected agencies

--- server/main.py
+++ server/main.py
@@ -48,7 +48,7 @@ def main():
                   f"listen_backlog: {listen_backlog} | logging_level: {logging_level}")
 
     # Initialize server and start server loop
-    server = Server(port, listen_backlog)
+    server = Server(port, listen_backlog, expected_agencies=int(os.getenv('EXPECTED_CLIENTS')))
     server.run()
 
 def initialize_log(logging_level):
